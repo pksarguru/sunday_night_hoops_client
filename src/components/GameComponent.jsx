3aca2764175b9a814d31f238a587c9db09187e83
Pavan Sarguru <pksarguru@users.noreply.github.com>
2018-08-05 16:11:38 -0500

    get hello world with GameComponent

---
+++ src/components/GameComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameComponent extends Component {
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    )
+  }
+}
+
+export default GameComponent;
